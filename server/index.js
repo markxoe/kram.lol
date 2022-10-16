db4c54093217535227412fd241d309915add3191
markxoe <mark@oude-elberink.de>
2022-10-16 12:43:15 +0200

    feat(server): allow cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
-const express = require("express");
+const cors = require("cors");
 const { Deta } = require("deta");
+const express = require("express");
 const app = express();
 
 const deta = Deta(process.env.PROJECT_KEY);
 const db = deta.Base("counter");
+
+app.use(cors({ origin: ["https://kram.lol", /localhost/] }));
 
 app.get("/", async (req, res) => {
   let val = (await db.get("view_count"))?.count ?? 0;
